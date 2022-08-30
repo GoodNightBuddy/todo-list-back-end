ce3b12e9f52ed63988d48c67648ef43d2d59fa0f
Pavel <fun.love.nuts@gmail.com@>
2022-08-30 14:54:29 +0300

    Added cors

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import { json } from 'body-parser';
 import express, {Request, Response, NextFunction} from 'express';
 import todoRoutes from './routes/todos';
+import cors from 'cors';
+// const cors=require("cors");
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            
+   optionSuccessStatus:200,
+};
 
 const app = express();
 
-app.use(json())
+app.use(json());
 
-app.use('/todos', todoRoutes)
+app.use(cors(corsOptions));
+
+app.use('/todos', todoRoutes);
 
 app.get('/', (req, res, next) => {
   res.end('<h1>Hello world</h1>')
